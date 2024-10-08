838f438a01c895aecdc9d978d398f2aab51c9c3a
avincigu <arthur.vinciguerra@ens-lyon.fr>
2024-10-08 17:41:08 +0200

    problem 1963

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod problem_0241;
 mod problem_1813;
 mod problem_2696;
 mod problem_0567;
+mod problem_1963;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/problem_1963.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_swaps(s: String) -> i32 {
+        let mut counter = 0;
+
+        for char in s.chars() {
+            if char == '[' {
+                counter+=1
+            }
+            else{
+                if counter > 0 {
+                    counter -= 1;
+                }
+            }
+        }
+
+        (counter+1)/2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::problem_1963::Solution;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(Solution::min_swaps("][][".to_string()), 1);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(Solution::min_swaps("]]][[[".to_string()), 2);
+    }
+}
